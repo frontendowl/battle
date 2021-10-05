5e2c2bb90e23c24cdb6bbfa2f463ad112f8d281e
ranjot <ranjotsd@gmail.com>
2021-10-05 16:37:50 +0100

    add form for names

--- app.rb
+++ app.rb
@@ -7,9 +7,14 @@ class Battle < Sinatra::Base
   end
 
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
   end
 
+  post '/names' do
+    @name1 = params[:name1]
+    @name2 = params[:name2]
+    erb :play
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,19 @@
 feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+  scenario 'Can load form for user to fill their name' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    page.find('form')
+  end
+end
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :name1, with: 'Bob'
+    fill_in :name2, with: 'Alice'
+    click_button 'Submit'
+    
+    save_and_open_page # will save the web page and open the browser to display it
+    
+    expect(page).to have_content 'Bob vs Alice'
   end
 end
